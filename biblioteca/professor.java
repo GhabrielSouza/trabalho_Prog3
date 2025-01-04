601754b5d3c3efc14f9d68c6c09284820e163ba4
Dhonathas <dhonathagoofins.filho@gmail.com>
2025-01-04 16:11:14 -0300

    Merge branch 'main' of https://github.com/GhabrielSouza/trabalho_Prog3 into dhonathas

--- biblioteca/professor.java
+++ biblioteca/professor.java
@@ -1,5 +1,7 @@
 package biblioteca;
 
+import javax.swing.JOptionPane;
+
 import divisao.Setor;
 
 class Professor extends Funcionario implements ILivroReservado {
@@ -18,11 +20,24 @@ class Professor extends Funcionario implements ILivroReservado {
 
     @Override
     public String informarReserva() {
-        if (reserva == null) {
-            return "Nenhuma reserva registrada.";
-        } else {
-            return "Reserva registrada: " + reserva.toString();
+        if (reservas == null || reservas.isEmpty()) {
+            String mensagemSemRegistro = "Nao ha reservas registradas.";
+            JOptionPane.showMessageDialog(null, mensagemSemRegistro, "Informacoes de Reservas", JOptionPane.INFORMATION_MESSAGE);
+            return mensagemSemRegistro;
         }
+
+        StringBuilder mensagem = new StringBuilder("Reservas do Professor:\n\n");
+
+        for (Reserva reserva : reservas) {
+            mensagem.append("Data de Reserva: ").append(reserva.getData()).append("\n");
+            for (Livro livro : reserva.getLivros()) {
+                mensagem.append("Livro: ").append(livro.getTitulo()).append("\n");
+            }
+            mensagem.append("\n");
+        }
+
+        JOptionPane.showMessageDialog(null, mensagem.toString(), "Informacoes de Reservas", JOptionPane.INFORMATION_MESSAGE);
+        return mensagem.toString();
     }
 
 }
